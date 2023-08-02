cc7fa674b45f6b262f19f948fc55d4bdca237cc5
schrodingho <15390486256@163.com>
2023-08-02 15:57:55 +0800

    finish printout pagetable

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void pgtprint(pagetable_t pagetable, int depth) {
+//     there are 2^9 = 512 PTEs in a page table.
+    for(int i = 0; i < 512; i++){
+        pte_t pte = pagetable[i];
+        if (pte & PTE_V) {
+            for(int j = 0; j < depth; j++){
+                printf(" ..");
+            }
+            printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+            if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+                uint64 child = PTE2PA(pte);
+                pgtprint((pagetable_t)child, depth + 1);
+            }
+        }
+    }
+//    kfree((void*)pagetable);
+}
+
+void vmprint(pagetable_t pagetable) {
+    printf("page table %p\n", pagetable);
+    pgtprint(pagetable, 1);
+}
